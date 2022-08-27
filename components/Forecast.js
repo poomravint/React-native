5fce5e6d51dfd9d08c197e8e43559243063fd01f
poomravint <101710514+poomravint@users.noreply.github.com>
2022-08-27 21:43:33 +0700

    Lastpush

--- components/Forecast.js
+++ components/Forecast.js
@@ -4,19 +4,29 @@ import React from "react";
 const Forecast = ({ main, description, temp }) => {
   return (
     <View style={{flex:1, justifyContent:'space-evenly'}}>
-      <Text style={styles.content}>{main}</Text>
-      <Text style={styles.content}>{description}</Text>
-      <Text style={styles.content}>{temp} °C</Text>
+      <Text style={styles.Text}>Main</Text>
+      <Text style={styles.Text2}>{main}</Text>
+      <Text style={styles.Text}>Description</Text>
+      <Text style={styles.Text2}>{description}</Text>
+      <Text style={styles.Text}>Temperature</Text>
+      <Text style={styles.Text2}>{temp} °C</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  content: {
-    fontSize: 20,
-    color: "white",
-    textAlign: "center"
-  },
+    Text: {
+        fontSize: 15 ,
+        textAlign: 'center' ,
+        paddingTop: 5,
+        color: 'white'
+    },
+    Text2: {
+        fontSize: 20 ,
+        textAlign: 'center' ,
+        paddingTop: 5,
+        color: 'blue'
+    },
 });
 
 export default Forecast;
--- components/Weather.js
+++ components/Weather.js
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     alignItems: 'center',
     width: '100%',
-    height: '100%'
+    height: '100%',
     },
    });
    
